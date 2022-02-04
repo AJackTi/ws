6b01b3713eaa35bb66bec0e3136a9ed26c4100ef
AJackTi <dtrong97vn@gmail.com>
2022-02-04 19:35:13 +0700

    10. Sending messages

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -112,6 +112,11 @@ func ListenToWsChannel() {
 			response.ConnectedUsers = users
 			broadcastToAll(response)
 
+		case "broadcast":
+			response.Action = "broadcast"
+			response.Message = fmt.Sprintf("<strong>%s</strong>: %s", e.Username, e.Message)
+			broadcastToAll(response)
+
 		}
 
 		//response.Action = "Got here"
