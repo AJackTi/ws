a26e66dbfe06c6c9f8be37c654baa42c71d644ac
AJackTi <dtrong97vn@gmail.com>
2022-02-04 19:53:05 +0700

    12. What if the server goes away Reconnecting painlessly

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,5 +12,8 @@ func routes() http.Handler {
 	mux.Get("/", http.HandlerFunc(handlers.Home))
 	mux.Get("/ws", http.HandlerFunc(handlers.WsEndpoint))
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Get("/static/", http.StripPrefix("/static/", fileServer))
+
 	return mux
 }
